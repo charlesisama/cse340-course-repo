f176b288a70650d35bff15b0bc86d18d41693846
charlesisama <cisama@byupathway.edu>
2026-05-07 00:37:02 +0100

    Add initial views and partials for the CSE 340 Service Network
    - Created home.ejs for the homepage with a welcome message and logo.
    - Added organizations.ejs to list partner organizations with their logos and contact information.
    - Implemented footer.ejs for consistent footer across pages.
    - Developed header.ejs for navigation and page title setup.
    - Introduced projects.ejs to showcase upcoming service projects.
    - Added unityserve-logo.png and other images for branding.

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
 import express from 'express';
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express restart!');
+/**
+  * Configure Express middleware
+  */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+
+
+/**
+ * Routes
+ */
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });
+});
+
+app.get('/organizations', async (req, res) => {
+    const title = 'Our Partner Organizations';
+    res.render('organizations', { title });
+});
+
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects', { title });
 });
 
 app.listen(PORT, () => {
