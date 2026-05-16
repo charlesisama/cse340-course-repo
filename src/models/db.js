0c18d6b557f9afa7219e459a67d52588d1a2879e
charlesisama <cisama@byupathway.edu>
2026-05-16 23:35:18 +0100

    feat: simplify SSL configuration in database connection setup

--- src/models/db.js
+++ src/models/db.js
@@ -11,14 +11,13 @@ import { Pool } from 'pg';
  * The connection string format is:
  * postgresql://username:password@host:port/database
  */
-const isProduction = process.env.NODE_ENV === 'production';
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-
-    ssl: isProduction
-        ? { rejectUnauthorized: false }
-        : false
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
 /**
  * Common SSL Issue:
  *
